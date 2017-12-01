dd9a96b02df1eeeaaaa9fed1d20a43c857384cad
willfalcon <will@creativedistillery.com>
2017-12-01 15:44:08 -0600

    Done

--- assets/js/amj.js
+++ assets/js/amj.js
@@ -4,18 +4,22 @@ function thirdRow() {
   if ( $('#third_row').visible() ) {
     $('#3_1_content').removeClass('hide-left');
     console.log("Third row moved in");
+
   } else {
     setTimeout(thirdRow, 100);
   }
+
 }
+
 function start() {
 
   $('.box').delay(100).removeClass('hide-opacity');
 
   if ( $('#second_row').visible() ) {
+
     $('#2_1_content').removeClass('hide-right');
-    thirdRow();
     console.log("Second row moved in");
+
   } else {
     setTimeout(start, 100);
   }
@@ -24,4 +28,6 @@ function start() {
 
 if (mq.matches) {
   $(document).ready(start);
+  $(document).ready(thirdRow);
+
 }
